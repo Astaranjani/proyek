f47c9e81a55d34431bc536e205c5c842b136faa7
putriayu069 <putriidm22@gmail.com>
2025-05-13 02:56:56 +0700

    user

--- resources/views/keranjang.blade.php
+++ resources/views/keranjang.blade.php
@@ -11,41 +11,34 @@
 
 <body>
     {{-- Navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-light bg-light px-4">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="{{ asset('images/logo.jpg') }}" alt="E-Mebel Logo" height="40">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto align-items-center">
-                <li class="nav-item"><a class="nav-link" href="{{ url('dashboard') }}">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('profile') }}">Profil</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('Riwayat Pesanan') }}">Riwayat Pesanan</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('keranjang') }}">Lihat Keranjang</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Pesan</a></li>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light px-4">
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <img src="{{ asset('images/logo.jpg') }}" alt="E-Mebel Logo" height="40">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto"> <!-- Pindahkan ke tengah dengan mx-auto -->
+            <li class="nav-item"><a class="nav-link" href="{{ url('dashboard') }}">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('profile') }}">Profil</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('Riwayat Pesanan') }}">Riwayat Pesanan</a></li>
+        </ul>
+                <a class="nav-link" href="{{ route('keranjang') }}">
+                    <img src="{{ asset('images/keranjang.png') }}" alt="Keranjang" style="width: 37px; height: 23px;">
+                </a>
                 @auth
-                <li class="nav-item">
+                <class="nav-item">
                     <form method="GET" action="{{ route('logout') }}">
                         @csrf
                         <button type="submit" class="btn btn-link nav-link" style="padding: 0; border: none; background: none;">
-                            Logout
-                        </button>
+                        <img src="{{ asset('images/logout.png') }}" alt="Logout" style="width: 20px; height: 20px;">
+                    </button>
                     </form>
-                </li>
-                <li class="nav-item dropdown ms-2">
-                    <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="{{ asset('images/icon profil.png') }}" alt="Profil" class="rounded-circle" style="width: 35px; height: 35px; object-fit: cover;">
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
-                        <li class="dropdown-item text-center fw-semibold">{{ Auth::user()->name }}</li>
-                    </ul>
-                </li>
                 @endauth
-            </ul>
         </div>
     </nav>
+    
         <section class="keranjang-wrapper bg-light py-4">
             <div class="container">
                 <h4 class="mb-3 fw-bold">Keranjang Saya</h4>
